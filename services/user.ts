027dbe43cccee11b385ae9dad3c6317bedb101ed
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-03-23 20:07:24 -0400

    user signUp

--- services/user.ts
+++ services/user.ts
@@ -10,7 +10,26 @@ interface SignInResponse {
   data: User
 }
 
+interface SignUpData {
+  name: string;
+  email: string;
+  password: string;
+  password_confirmation: string;
+}
+
 const UsersService = {
+  signUp: ({ 
+    name, 
+    email, 
+    password, 
+    password_confirmation 
+  }: SignUpData) => 
+    api.post<void>('/auth/v1/user', {
+      name,
+      email,
+      password,
+      password_confirmation
+  }),
   signIn: ({ email, password }: SignInData) => 
     api.post<SignInResponse>('auth/v1/user/sign_in', {
       email,
